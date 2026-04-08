9e09cde05161c61df35c75e1eee0425e8b388a2d
Aquat1c <mitshaforever@gmail.com>
2026-04-08 21:47:57 +0300

    Version bump.

--- include/mod_version.h
+++ include/mod_version.h
@@ -2,7 +2,7 @@
 
 namespace netplay::build_info
 {
-inline constexpr const char* kVersion = "0.3.2";
+inline constexpr const char* kVersion = "0.3.3";
 inline constexpr const char* kBuildTimestamp = __DATE__ " " __TIME__;
 inline constexpr const char* kDisplayName = "In-game Netplay";
 }
